46aa874cd93df2766663f422fe346350d30fe9bc
lionjeongin <cindy93489163@gmail.com>
2024-04-11 12:23:08 +0900

    Revert "Merge pull request #1 from lionjeongin/setting"
    This reverts commit 548ffedf568e468630a339ed7994886f25982c0e, reversing
    changes made to d3c05c998e350bc73d60d112e09390e3735688f8.

--- src/langs/en/index.js
+++
@@ -1,7 +0,0 @@
-import commons from "./commons";
-import validations from "./validations";
-import errors from "./errors";
-
-const en = { ...commons, ...validations, ...errors };
-
-export default en;
