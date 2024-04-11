b72fe0301f4808af088134d84dbe0fd80073c7f2
lionjeongin <cindy93489163@gmail.com>
2024-04-11 15:31:09 +0900

    라우팅 구성 완료

--- src/commons/pages/Error.js
+++ src/commons/pages/Error.js
@@ -1,25 +1,26 @@
-import React, {Component} from "react";
-import loadable from "@loadable/component";
+import React, { Component } from 'react';
+import loadable from '@loadable/component';
 
 const ErrorDisplay = loadable(() => import('../components/ErrorDisplay'));
 
 class ErrorPage extends Component {
-    state = {
-        message: '',
-    };
-    componentDidCatch(error, info){
-        if (error) {
-            this.setState({ message: error.message});
-            console.error(error, info);
-        }
-    }
+  state = {
+    message: '',
+  };
 
-    render() {
-        const { children } = this.props;
-        const { message } = this.state;
-
-        return message ? <ErrorDisplay>{message}</ErrorDisplay> : children;
+  componentDidCatch(error, info) {
+    if (error) {
+      this.setState({ message: error.message });
+      console.error(error, info);
     }
+  }
+
+  render() {
+    const { children } = this.props;
+    const { message } = this.state;
+
+    return message ? <ErrorDisplay>{message}</ErrorDisplay> : children;
+  }
 }
 
 export default React.memo(ErrorPage);
